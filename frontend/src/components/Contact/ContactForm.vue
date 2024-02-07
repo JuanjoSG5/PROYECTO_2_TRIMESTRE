579d656532ab4973f6421902d887ee7ea2d5273b
Juan Jose Sanchez Gonzalez <113671548+JuanjoSG5@users.noreply.github.com>
2024-02-07 20:09:42 +0100

    feat: Added validation to the contact form

--- frontend/src/components/Contact/ContactForm.vue
+++ frontend/src/components/Contact/ContactForm.vue
@@ -1,13 +1,28 @@
 <template>
     <form class="form-control" @submit.prevent="submitForm">
       <legend>This is the contact Form</legend>
-      <CustomInput label="Inquiry Type" type="select" v-model="selectedInquiry" :options="possibleInquiries" :formSent="formSent" />
-      <CustomInput label="Description of the inquiry" type="textarea" v-model="userText" placeholder="Write here what you want to communicate to us" :formSent="formSent" />
+      <CustomInput 
+        label="Inquiry Type" type="select" 
+        v-model="selectedInquiry"
+        :options="possibleInquiries"
+        :formSent="formSent" 
+        :errorMessage="selectedInquiryErrorMessage"
+        @input="clearSelectedInquiryErrorMessage"
+      />
+      <CustomInput 
+        label="Description of the inquiry" 
+        type="textarea" 
+        v-model="userText"
+        placeholder="Write here what you want to communicate to us"
+        :formSent="formSent"
+        :errorMessage="userTextErrorMessage"
+        @input="clearUserTextErrorMessage"
+      />
       <SubmitButton @click="submitForm"/>
       <h3 v-if="formSent">The inquiry has been submitted</h3>
     </form>
   </template>
-  
+    
   <script>
   import CustomInput from '../shared/helpers/GenericInput.vue';
   import SubmitButton from '../shared/helpers/SubmitButton.vue';
@@ -20,24 +35,48 @@
     data() {
       return {
         formSent: false,
-        selectedInquiry: '', 
+        selectedInquiry: '',
         userText: '',
-        possibleInquiries: [    
+        possibleInquiries: [
           { label: 'General', value: 'General Inquiry' },
           { label: 'Bug', value: 'Application malfuction' },
           { label: 'Assistance', value: 'assistance' },
-          { label: 'Other', value: 'Other Inquiry'}
-        ]
+          { label: 'Other', value: 'Other Inquiry' }
+        ],
+        selectedInquiryErrorMessage: '',
+        userTextErrorMessage: ''
       };
     },
     methods: {
       submitForm() {
-        this.formSent = true;
+        this.selectedInquiryErrorMessage = '';
+        this.userTextErrorMessage = '';
+  
+        if (this.selectedInquiry.trim() === '') {
+          this.selectedInquiryErrorMessage = 'Please select an inquiry type';
+        }
+        if (this.userText.trim() === '') {
+          this.userTextErrorMessage = 'Please enter a valid description';
+        }
+  
+        if (!this.selectedInquiryErrorMessage && !this.userTextErrorMessage) {
+          this.formSent = true;
+          // Reset formSent after successful submission
+          setTimeout(() => {
+            this.formSent = false;
+          }, 3000); // Reset after 3 seconds (adjust as needed)
+        }
+      },
+      clearSelectedInquiryErrorMessage() {
+        this.selectedInquiryErrorMessage = '';
+      },
+      clearUserTextErrorMessage() {
+        this.userTextErrorMessage = '';
       }
     }
   };
   </script>
-  
+    
   <style scoped>
   .form-control {
     display: flex;
--- frontend/src/components/shared/helpers/GenericForm.vue
+++ frontend/src/components/shared/helpers/GenericForm.vue
@@ -1,64 +1,64 @@
 <template>
-    <form class="form-control" >
-      <slot :validateForm="validateForm"></slot>
-      <SubmitButton />
+    <form class="form-control">
+        <slot :validateForm="validateForm"></slot>
+        <SubmitButton @click="handleSubmit" />
     </form>
-  </template>
+</template>
   
-  <script>
-  import SubmitButton from './SubmitButton.vue';
-  
-  export default {
+<script>
+import SubmitButton from './SubmitButton.vue';
+
+export default {
     components: {
-      SubmitButton
+        SubmitButton
     },
     props: {
-      validationRules: {
-        type: Object,
-        required: true
-      }
+        validationRules: {
+            type: Object,
+            required: true
+        }
     },
     data() {
-      return {
-        formData: {}
-      };
+        return {
+            formData: {}
+        };
     },
     methods: {
-      getErrorMessage(fieldName) {
-        return this.validationRules[fieldName].errorMessage;
-      },
-      validateForm() {
-        let correctForm = true;
-        for (const fieldName in this.validationRules) {
-          if (!this.validateField(fieldName)) {
-            correctForm = false;
-          }
+        getErrorMessage(fieldName) {
+            return this.validationRules[fieldName].errorMessage;
+        },
+        validateForm() {
+            let correctForm = true;
+            for (const fieldName in this.validationRules) {
+                if (!this.validateField(fieldName)) {
+                    correctForm = false;
+                }
+            }
+            return correctForm;
+        },
+        validateField(fieldName) {
+            const rule = this.validationRules[fieldName];
+            if (!rule.validator(this.formData[fieldName])) {
+                rule.errorMessage = `Please enter a valid ${rule.label}.`;
+                return false;
+            }
+            return true;
+        },
+        handleSubmit() {
+            if (this.validateForm()) {
+                this.$emit('submit', this.formData);
+            }
         }
-        return correctForm;
-      },
-      validateField(fieldName) {
-        const rule = this.validationRules[fieldName];
-        if (!rule.validator(this.formData[fieldName])) {
-          rule.errorMessage = `Please enter a valid ${rule.label}.`;
-          return false;
-        }
-        return true;
-      },
-      handleSubmit() {
-        if (this.validateForm()) {
-          this.$emit('submit', this.formData);
-        }
-      }
     }
-  };
-  </script>
+};
+</script>
   
-  <style scoped>
-  .form-control {
+<style scoped>
+.form-control {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
 }
-  </style>
+</style>
   
