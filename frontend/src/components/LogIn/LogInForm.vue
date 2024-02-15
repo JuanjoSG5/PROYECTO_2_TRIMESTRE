b59550b58d693c6155e41581681c074677c1104b
Juan Jose Sanchez Gonzalez <113671548+JuanjoSG5@users.noreply.github.com>
2024-02-15 08:37:54 +0100

    feat:implemented Sign Up page

--- frontend/src/components/LogIn/LogInForm.vue
+++ frontend/src/components/LogIn/LogInForm.vue
@@ -1,8 +1,9 @@
 <template>
     <CustomForm :formData="formData" :validationRules="validationRules" >
         <legend class="form-title"> Log In</legend>
-        <p class="signup-suggestion">Don’t have an count yet </p>
-        <CustomInput 
+        <p class="signup-suggestion">Don’t have an count yet 
+            <router-link v-if="!isLoggedIn" class="route" to="/register">Sign Up</router-link>
+        </p><CustomInput 
             label="Username" 
             v-model="formData.username" 
             placeholder="Username"
@@ -18,14 +19,7 @@
             @input="handleInputChange('password', $event.target.value)" 
         />
 
-        <CustomInput 
-            label="Email" 
-            type="email" 
-            v-model="formData.email" 
-            placeholder="example@gmail.com"
-            :errorMessage="errorMessage.email" 
-            @input="handleInputChange('email', $event.target.value)" 
-        />
+        
         
         
     </CustomForm>
@@ -35,7 +29,7 @@
 // Import necessary components and methods
 import CustomInput from '../shared/helpers/GenericInput.vue';
 import CustomForm from '../shared/helpers/GenericForm.vue';
-import { validateEmail, validatePassword, validateUsername } from '../shared/helpers/InputValidation.js';
+import { validatePassword, validateUsername } from '../shared/helpers/InputValidation.js';
 
 export default {
     components: {
@@ -48,12 +42,10 @@ export default {
             formData: {
                 username: '',
                 password: '',
-                email: ''
             },
             errorMessage: {
                 username: '',
                 password: '',
-                email: ''
             },
             validationRules: {
                 username: {
@@ -64,10 +56,6 @@ export default {
                     label: 'Password',
                     validator: (value) => validatePassword(value)
                 },
-                email: {
-                    label: 'Email',
-                    validator: (value) => validateEmail(value) 
-                }
             }
         };
     },
@@ -95,6 +83,16 @@ export default {
     
     .signup-suggestion{
         font-size: 1.5rem;
+    }
+    .route{
+        text-decoration: none;
+        font-weight: bold;
+        border-radius: .5rem;
+        transition: background-color 0.8s, color 0.8s;
+        &:hover{
+            background-color: var(--vt-c-black-mute);
+            color: var(--vt-c-black);
+        }
     }
 </style>
   
--- frontend/src/components/SignUp/SignUpForm.vue
+++ frontend/src/components/SignUp/SignUpForm.vue
@@ -1,13 +1,112 @@
-<template lang="">
-    <div>
-        -sign up 
-    </div>
+<template>
+    <CustomForm :formData="formData" :validationRules="validationRules" >
+        <legend class="form-title">Sign Up</legend>
+        <p class="signup-suggestion">Already have an account?
+            <router-link v-if="!isLoggedIn" class="route" to="/login">Log In</router-link>
+        </p>
+        <CustomInput 
+            label="Username" 
+            v-model="formData.username" 
+            placeholder="Username"
+            :errorMessage="errorMessage.username" 
+            @input="handleInputChange('username', $event.target.value)"
+        />
+
+        <CustomInput 
+            label="Password" 
+            type="password" 
+            v-model="formData.password"
+            :errorMessage="errorMessage.password" 
+            @input="handleInputChange('password', $event.target.value)" 
+        />
+
+        <CustomInput 
+            label="Email" 
+            type="email" 
+            v-model="formData.email" 
+            placeholder="example@gmail.com"
+            :errorMessage="errorMessage.email" 
+            @input="handleInputChange('email', $event.target.value)" 
+        />
+        
+        
+    </CustomForm>
 </template>
+
 <script>
+// Import necessary components and methods
+import CustomInput from '../shared/helpers/GenericInput.vue';
+import CustomForm from '../shared/helpers/GenericForm.vue';
+import { validateEmail, validatePassword, validateUsername } from '../shared/helpers/InputValidation.js';
+
 export default {
-    
-}
+    components: {
+        CustomInput,
+        CustomForm
+    },
+    data() {
+        return {
+            
+            formData: {
+                username: '',
+                password: '',
+                email: ''
+            },
+            errorMessage: {
+                username: '',
+                password: '',
+                email: ''
+            },
+            validationRules: {
+                username: {
+                    label: 'Username',
+                    validator: (value) => validateUsername(value) 
+                },
+                password: {
+                    label: 'Password',
+                    validator: (value) => validatePassword(value)
+                },
+                email: {
+                    label: 'Email',
+                    validator: (value) => validateEmail(value) 
+                }
+            }
+        };
+    },
+    methods: {
+        
+        handleInputChange(fieldName, value) {
+            console.log(fieldName, value);
+            this.formData[fieldName] = value; 
+            this.errorMessage[fieldName] = '';
+        },
+
+        getErrorMessage(fieldName) {
+            return this.validationRules[fieldName].errorMessage;
+        }
+    }
+};
 </script>
-<style lang="">
+  
+<style scoped>
     
+    .form-title{
+        font-size: 3rem;
+        font-weight: bold;
+    }
+    
+    .signup-suggestion{
+        font-size: 1.5rem;
+    }
+    .route{
+        text-decoration: none;
+        font-weight: bold;
+        border-radius: .5rem;
+        transition: background-color 0.8s, color 0.8s;
+        &:hover{
+            background-color: var(--vt-c-black-mute);
+            color: var(--vt-c-black);
+        }
+    }
 </style>
+  
